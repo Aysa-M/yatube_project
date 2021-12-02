f0c464cc4f1e4a05c917bba7d5e1bc1a6e78ad31
Aysa Matsakova <happiness390@gmail.com>
2021-12-03 00:09:47 +0300

    Добавлены urls и view-функции.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
+def index(request):
+    """Information which is showing up on the start page."""
+    return HttpResponse('FROM PEOPLE TO PEOPLE. '
+                        'SHARE YOUR IDEAS, '
+                        'THOUGHTS, OPINIONS, ARTS OF WORDS '
+                        'AND COMPLAININGS HERE! '
+                        'BLOGSPOT IS WAITING FOR!')
+
+
+def group_posts(request, any_slug):
+    """Information for displaying on the page with posts grouped by GROUPS."""
+    return HttpResponse(f'HERE YOU CAN CHOSE THE MOST LIKED TOPICS OF YOURS. '
+                        f'OPEN ANY {any_slug} AND ENJOY.')
